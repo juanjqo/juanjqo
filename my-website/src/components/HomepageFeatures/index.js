e09ed05812f61e81a6f2c3e29c5ab9e9741a14f9
Juan Jose Quiroz Omana <juanjqo@g.ecc.u-tokyo.ac.jp>
2022-07-21 10:14:01 +0900

    Updated the photo.

--- my-website/src/components/HomepageFeatures/index.js
+++ my-website/src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Juan J. Quiroz O.',
-    Svg: require('@site/static/img/photo.svg').default,
+    Svg: require('@site/static/img/photo_2.svg').default,
     description: (
       <>
         I'm a robotics researcher at The University of Tokyo.
