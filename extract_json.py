470e8aae5c4835bc51b3dfb4c283bb971779419c
Juancho <juanjqogm@gmail.com>
2024-04-05 16:30:02 -0500

    Fixed the extracter feature

--- extract_json.py
+++ extract_json.py
@@ -1,3 +1,14 @@
 import shutil
+import os
+from submodules.serp_api_extracter.utils.utils import extract_json_and_save
+#shutil.copyfile('./submodules/serp_api_extracter/data.json', 'google_scholar_juanjqo.json')
 
-shutil.copyfile('./submodules/serp_api_extracter/data.json', 'google_scholar_juanjqo.json')
+
+def main() -> None:
+    base_path = os.getcwd()
+    my_json = base_path + '/' + 'google_scholar_juanjqo.json'
+    extract_json_and_save(my_json)
+
+
+if __name__ == "__main__":
+    main()
